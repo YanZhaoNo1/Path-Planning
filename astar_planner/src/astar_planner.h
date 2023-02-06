a5619727175f9d26dff876ca878940c742020a7f
YanZhaoNo1 <122534367+YanZhaoNo1@users.noreply.github.com>
2023-02-06 22:06:20 +0800

    江波学长去年写的代码

--- astar_planner/src/astar_planner.h
+++ astar_planner/src/astar_planner.h
@@ -1,8 +1,12 @@
+
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
 #include <ros/ros.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <costmap_2d/costmap_2d.h>
 #include <nav_core/base_global_planner.h>
 
+#include "pathSplineSmoother/pathSplineSmoother.h"
+#include<tf/transform_broadcaster.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Path.h>
 
@@ -17,7 +21,10 @@ using namespace std;
 struct Node{
   float cost;
   int index;
+  //int id;
 };
+
+
 
 namespace astar_planner {
     class AstarPlanner : public nav_core::BaseGlobalPlanner{
@@ -45,7 +52,7 @@ namespace astar_planner {
       int width;
       int height;
       int map_size;
-      vector<bool> OGM;
+     //vector<bool> OGM;
 
       double getHeuristic(int cell_index, int goal_index);
       
@@ -57,11 +64,39 @@ namespace astar_planner {
       * @brief  Publish a path for visualization purposes
       */
       void publishPlan(const std::vector<geometry_msgs::PoseStamped>& path);
-
+        
       ros::Publisher plan_pub_;
       std::string frame_id_;
-	  bool initialized_;
+	  bool initialized_=false;
 	  costmap_2d::Costmap2DROS* costmap_ros_;
 	  costmap_2d::Costmap2D* costmap_;
+
+    //heuristic_function_
+    std::string heuristic_name_;
+
+    //! heuristic_factor_  启发系数
+    float heuristic_factor_;
+    float cost_weight;
+    float ymovecost;
+    float xmovecost;
+    float yymovecost;
+    float xxmovecost;
+
+     //! inaccessible_cost
+  float inaccessible_cost_;
+
+  //! goal_search_tolerance
+  float  goal_search_tolerance_;
+
+    double move_cost;
+    //! gridmap cost array
+  unsigned char *cost_;
+
+  vector<RealPoint> SmoothPlan(vector<int> path);
+ bool smooth;
+PathSplineSmoother *spline_smoother_;
+int smooth_size_;
+ std::string tf_prefix_;
+double costnum;
     };
 };
